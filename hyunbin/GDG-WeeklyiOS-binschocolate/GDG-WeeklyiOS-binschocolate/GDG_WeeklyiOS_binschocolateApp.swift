d334384b85f4c9aba8ce1fd5a5d9fbada1e10939
binschocolate <hisbulletp@gmail.com>
2024-10-08 19:20:39 +0900

    Week3
    박현빈

---
+++ hyunbin/GDG-WeeklyiOS-binschocolate/GDG-WeeklyiOS-binschocolate/GDG_WeeklyiOS_binschocolateApp.swift
@@ -0,0 +1,17 @@
+//
+//  GDG_WeeklyiOS_binschocolateApp.swift
+//  GDG-WeeklyiOS-binschocolate
+//
+//  Created by 박현빈 on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct GDG_WeeklyiOS_binschocolateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Week3()
+        }
+    }
+}
