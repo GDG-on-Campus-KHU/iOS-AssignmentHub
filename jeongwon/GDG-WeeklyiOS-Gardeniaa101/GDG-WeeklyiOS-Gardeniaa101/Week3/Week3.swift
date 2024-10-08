8ea9f0cd065c4fcc5d77b09303bbc27bdba67c7c
Gardeniaa101 <onejwon33@naver.com>
2024-10-08 19:17:04 +0900

    week3
    Setting up a development environment

---
+++ jeongwon/GDG-WeeklyiOS-Gardeniaa101/GDG-WeeklyiOS-Gardeniaa101/Week3/Week3.swift
@@ -0,0 +1,18 @@
+//
+//  Week3_Practice.swift
+//  GDG-WeeklyiOS-Gardeniaa101
+//
+//  Created by HanJW on 10/8/24.
+//
+
+import SwiftUI
+
+struct Week3_Practice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Week3_Practice()
+}
